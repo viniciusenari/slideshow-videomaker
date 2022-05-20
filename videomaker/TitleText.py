a77d4d74b4c50c1f3f644c08d8b5d153c0386151
Vinicius Koji Enari <viniciusenari@gmail.com>
2022-05-20 19:30:31 -0300

    Add title text to beginning of video
    Add a clip with the title of the video to the beginning.

--- videomaker/TitleText.py
+++ videomaker/TitleText.py
@@ -1,11 +1,19 @@
 import moviepy.editor as mpy
-import os
 
+def create_title_text_clip(text):
+    title_text = mpy.TextClip(txt = text,
+                    font = "Candara-Bold",
+                    size= (800, 0),
+                    color = 'white')
+    title_text = title_text.set_position("center")
 
-title = "Divisão dos Universitários, RM Pinda"
-title_text = mpy.TextClip(txt = title,
-                        fontsize = 40,
-                        color = 'white',
-                        bg_color = 'blue')
+    tt_width, tt_height = title_text.size
 
-title_text.save_frame("test.png")
+    background_frame = mpy.ColorClip(size=(tt_width + 100, tt_height + 50), color = (16, 78, 139))
+    background_frame = background_frame.set_position("center")
+
+    background_color = mpy.ColorClip(size=(1280, 720), color = (0, 0, 0))
+
+    final_clip = mpy.CompositeVideoClip([background_color, background_frame, title_text])
+    
+    return final_clip
